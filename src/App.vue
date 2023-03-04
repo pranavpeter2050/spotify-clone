500d22e2345cddc9741307c3594825ee1e7715aa
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-03-04 21:35:13 +0530

    Main Layout Section.
    - Added assets (images, songs)

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,93 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import  { RouterLink, RouterView } from 'vue-router'
+
+import ChevronUp from 'vue-material-design-icons/ChevronUp.vue';
+import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+import ChevronRight from 'vue-material-design-icons/ChevronRight.vue';
+
+import MenuItem from './components/MenuItem.vue'
+
+let openMenu = ref(false)
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="
+      w-[calc(100%-240px)]
+      h-[60px]
+      fixed
+      right-0
+      z-20
+      bg-[#101010]
+      bg-opacity-80
+      flex
+      items-center
+      justify-between"
+    >
+      <div class="flex items-center ml-6">
+        <button type="button" class="rounded-full bg-black p-[1px] cursor-pointer">
+          <ChevronLeft fillColor="#FFFFFF" :size="30" />
+        </button>
+        <button type="button" class="rounded-full bg-black p-[1px] ml-4 cursor-pointer">
+          <ChevronRight fillColor="#FFFFFF" :size="30" />
+        </button>
+      </div>
+
+      <button
+        @click="openMenu = !openMenu" :class="openMenu ? 'bg-[#282828]' : 'bg-black'"
+        class="bg-black hover:bg-[#282828] rounded-full p-0.5 mr-8 mt-0.5 cursor-pointer"
+      >
+        <div class="flex items-center">
+          <img
+            class="rounded-full"
+            width="27"
+            src="https://yt3.ggpht.com/e9o-24_frmNSSVvjS47rT8qCHgsHNiedqgXbzmrmpsj6H1ketcufR1B9vLXTZRa30krRksPj=s88-c-k-c0x00ffffff-no-rj-mo" 
+          />
+          <div class="text-white text- ml-1 5 font-semibold">Pranav Peter</div>
+          <ChevronDown v-if="!openMenu" @click="openMenu = true" fillColor="#FFFFFF" :size="25" />
+          <ChevronUp v-else @click="openMenu = false" fillColor="#FFFFFF" :size="25" />
+        </div>
+      </button>
+
+      <span 
+        v-if="openMenu"
+        class="
+          fixed
+          w-[190px]
+          bg-[#282828]
+          shadow-2xl
+          z-50
+          rounded-sm
+          top-[52px]
+          right-[35px]
+          p-1
+          cursor-pointer
+        "
+      >
+        <ul class="text-gray-200 font-semibold text-[14px]">
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D] border-b border-b-gray-600">Profile</li>
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D]">Log Out</li>
+        </ul>
+      </span>
+    </div>
+
+    <div 
+      id="SideNav"
+      class="h-[100%] p-6 w-[240px] fixed z-50 bg-black"
+    >
+      <RouterLin to="/">
+        <img width="125" src="/images/icons/spotify-logo.png" alt="Spotify">
+      </RouterLin>
+      <div class="my-8"></div>
+      <ul>
+        <RouterLin to="/">
+          <MenuItem />
+        </RouterLin>
+      </ul>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-  <h1 class="text-3xl font-bold underline mt-5 text-pink-500">
-    Tailwind Installed!
-  </h1>
 </template>
 
 <style scoped>
