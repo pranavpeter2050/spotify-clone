38cb4b311ac65a9363ccd46adaead4b3626ebf27
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-03-04 22:21:07 +0530

    Main Layout Section: Fixed error when using "route.path" in MenuItem.vue

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import App from './App.vue'
 
 import { createPinia } from 'pinia'
 import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
+import router from './router'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedState)
 
 const app = createApp(App)
 
-app.use(pinia)
+app.use(createPinia())
+app.use(router)
 
 app.mount('#app')
