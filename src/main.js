8f1e9bfbd91cc0009885687282cb822a1be64a10
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-03-04 19:34:53 +0530

    Configuring pinia
    Vue Router setting at: https://youtu.be/ZzuqwqP1wAQ?t=454

--- src/main.js
+++ src/main.js
@@ -2,4 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createPinia } from 'pinia'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
+
+const app = createApp(App)
+
+app.use(pinia)
+
+app.mount('#app')
