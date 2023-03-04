811fdfba9eb0adefb28866c0c2a91400d6186e98
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-03-05 00:15:09 +0530

    Home Page; Also created HomeCard component.

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import App from '../App.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: App
+      component: HomeView
     },
   ]
 })
