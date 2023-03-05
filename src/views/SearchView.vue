8e806db3bdbb91b46a25a1dccbcc1fa660b20ce1
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-03-05 11:38:23 +0530

    Search Page: Created SearchView file.

---
+++ src/views/SearchView.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+	<div class="p-8">
+		<button type="button" class="text-white text-2xl font-semibold hover:underline cursor-pointer">
+			Podcasts to make to Smarter
+		</button>
+
+		<div class="py-1.5"></div>
+
+		<div class="flex items-center">
+			<CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300" />
+		</div>
+	</div>
+</template>
